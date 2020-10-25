4acbe25f57bc33a5f917d03d1821d61fc34f0279
Jens Pettersson <jens.pettersson@ingka.com>
2020-10-25 21:48:55 +0100

    Add my private repository to script

--- Docker/create_images.sh
+++ Docker/create_images.sh
@@ -4,7 +4,7 @@
 # For Ubuntu build, please change to Ubuntu Docerfile
 
 docker build -t jmeter-base:latest -f Dockerfile-base-centos .
-docker build -t jmeter-master:latest -f Dockerfile-master .
-docker build -t jmeter-slave:latest -f Dockerfile-slave .
+docker build -t mesmeratu/jmeter-master:latest -f Dockerfile-master .
+docker build -t mesmeratu/jmeter-slave:latest -f Dockerfile-slave .
 
 
